bac1177cd9253550da9a2594c2deaffcef178d9d
Adrian Kokot <kokotadriann@gmail.com>
2023-03-10 21:25:14 +0100

    wip

--- helpers/image_exec.py
+++ helpers/image_exec.py
@@ -16,6 +16,11 @@ class ImageExec:
       # raise Exception(f'Point ({x}, {y}) out of image!!!')
     self.image[y][x] = color
 
+  def get_pixel(self, x, y):
+    if (x >= self.width or y >= self.height or x < 0 or y < 0):
+      return (None, None, None)
+    return self.image[y][x]
+
   def draw_bresenham_line(self, point1, point2, color):
     x1, y1 = point1
     x2, y2 = point2
@@ -73,9 +78,12 @@ class ImageExec:
       if y2 < y1:
         y1, y2 = (y2, y1)
       for y in range(y1, y2 + 1):
-        sum += self.image[y][x1][0]
-        px += 1
-        self.set_pixel(x1, y, (255, 0, 0))
+        pixel = self.get_pixel(x1, y)[0]
+
+        if (pixel):
+          sum += pixel
+          px += 1
+          self.set_pixel(x1, y, (255, 0, 0))
     else:
       m = float(rise) / run
       adjust = 1 if m >= 0 else -1
@@ -89,9 +97,13 @@ class ImageExec:
           x1, x2 = (x2, x1)
           y = y2
         for x in range(x1, x2 + 1):
-          sum += self.image[y][x][0]
-          px += 1
-          self.set_pixel(x, y, (0, 0, 255))
+          pixel = self.get_pixel(x, y)[0]
+
+          if (pixel):
+            sum += pixel
+            px += 1
+            self.set_pixel(x, y, (0, 0, 255))
+
           offset += delta
           if offset >= threshold:
             y+= adjust
@@ -103,12 +115,19 @@ class ImageExec:
           y1, y2 = (y2, y1)
           x = x2
         for y in range(y1, y2 + 1):
-          sum += self.image[y][x][0]
-          px += 1
+          pixel = self.get_pixel(x, y)[0]
+
+          if (pixel):
+            sum += pixel
+            px += 1
+            self.set_pixel(x, y, (0, 255, 0))
+
           offset += delta
-          self.set_pixel(x, y, (0, 255, 0))
           if offset >= threshold:
             x += adjust
             threshold += 1
 
-    return sum, px
+
+    if px == 0:
+        return 0, 0, 0
+    return sum, px, sum / px
